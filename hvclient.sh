a2615783584b2c592cd106a5a9b75ffa228de764
PrashantFAE <88538314+PrashantGSIN@users.noreply.github.com>
2021-10-28 15:10:50 +0530

    Create hvclient.sh

---
+++ hvclient.sh
@@ -0,0 +1,8 @@
+vclient -claimsubmit=example.com
+# hvclient -trustchain
+# openssl genrsa 2048 > test.key
+# hvclient -privatekey test.key -commonname Demo_cert -csrout > csr.pem
+# openssl req -text -noout -in csr.pem
+# hvclient -claimdns="3AEA9E9966E8774203A20C574B4C5C5D"
+# hvclient -commonname Demo_cert -dnsnames example.com -csr csr.pem > cert1.pem
+# cat cert.pem
