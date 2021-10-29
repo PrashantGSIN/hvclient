cf81107ac8fd3aef8ee52917de2457758b7281a4
PrashantFAE <88538314+PrashantGSIN@users.noreply.github.com>
2021-10-29 14:59:28 +0530

    Update hvclient.sh

--- hvclient.sh
+++ hvclient.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 hvclient -claimsubmit=example.com
+hvclient -generate -publickey="testdata/rsa_pub.key"
+hvclient -generate -duration="30d" -privatekey="testdata/rsa_priv.key"
+hvclient -generate -notbefore="2021-10-19T14:30:00IST" -notafter="2021-10-29T18:00:00IST" -csr="testdata/request.p10"
+hvclient -generate -notbefore="2021-10-18T14:30:00IST" -duration="90d" -privatekey="testdata/ec_priv.key" -gencsr
 # hvclient -trustchain
 # openssl genrsa 2048 > test.key
 # hvclient -privatekey test.key -commonname Demo_cert -csrout > csr.pem
