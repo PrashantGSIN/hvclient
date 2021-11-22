175678537a09bdda940fd6718ce95ed26edd8af5
PrashantFAE <88538314+PrashantGSIN@users.noreply.github.com>
2021-11-22 15:33:40 +0530

    Update hvclient.sh

--- hvclient.sh
+++ hvclient.sh
@@ -20,8 +20,13 @@ echo "Token to put into DNS: ${token}"
 echo "The claimID: ${claim_id}"
 
 hvclient -claimdns="${claim_id}"
-hvclient -generate -publickey="testdata/rsa_pub.key"
-hvclient -generate -duration="30d" -privatekey="testdata/rsa_priv.key"
-hvclient -generate -notbefore="2021-11-16T10:00:00IST" -notafter="2021-11-16T18:00:00IST" -csr="testdata/request.p10"
-hvclient -generate -notbefore="2021-11-16T10:00:00IST" -duration="90d" -privatekey="testdata/ec_priv.key" -gencsr
-openssl genrsa 2048 > test1.key
+# hvclient -generate -publickey="testdata/rsa_pub.key"
+# hvclient -generate -duration="30d" -privatekey="testdata/rsa_priv.key"
+# hvclient -generate -notbefore="2021-11-16T10:00:00IST" -notafter="2021-11-16T18:00:00IST" -csr="testdata/request.p10"
+# hvclient -generate -notbefore="2021-11-16T10:00:00IST" -duration="90d" -privatekey="testdata/ec_priv.key" -gencsr
+# openssl genrsa 2048 > test1.key
+
+openssl genrsa 2048 > test.key
+hvclient -privatekey test.key -commonname Demo_cert -csrout > csr.pem
+openssl req -text -noout -in csr.pem
+hvclient -commonname Demo_cert -csr csr.pem | openssl x509 -text -noout
